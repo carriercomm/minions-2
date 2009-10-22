63c8b74d02d060e27a986fae2d0dda54db019435
ckode <ckode@2e7fb659-bb13-453e-afb5-4fb98f41aec3>
2009-10-22 19:29:50 +0000

    Cleaned up dbsetup.sql and split the data load to data.sql
    git-svn-id: https://xp-dev.com/svn/Minions@4 2e7fb659-bb13-453e-afb5-4fb98f41aec3

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO rooms VALUES( 1, "Room dia Muertos", "The stale air and dank stone make it difficult to breathe. Bones lay piled, who knows how deep they are. Many have died here and more come each day.",0,0,0,0,0,0,0,0,2,0);
+
+
+INSERT INTO races VALUES( 1, "Human", "The Human is the most balanced race at this time.");
+INSERT INTO races VALUES( 1, "Dark-Elf", "The Dark-Elves are a darker relative of the Elf.");
+INSERT INTO races VALUES( 2, "Dwarf", "The Dwarves are a stout and healthy race, what they lack in height they make up for with sturdiness and brawn.");
+
+
+INSERT INTO classes VALUES( 1, "Fighter", "Physical Combat is thier forte.");
+INSERT INTO classes VALUES( 2, "Ranger", "Drawn to the woodlands and thier protection, Rangers gain special powers from Nature.");
+INSERT INTO classes VALUES( 1, "Cleric", "Physical Combat is thier holy calling, and healing thier forte.");
+INSERT INTO classes VALUES( 1, "Mage", "Frail physically but able to concentrate the mysterious energies into deadly forces.");
--- dbsetup.sql
+++ dbsetup.sql
@@ -1,16 +1,31 @@
-create table rooms( RoomNumber bigint primary key, ShortDesc varchar(82), LongDesc varchar(322), Dir1 bigint, Dir2 bigint, Dir3 bigint, Dir4 bigint, Dir5 bigint, Dir6 bigint, Dir7 bigint, Dir8 bigint, Dir9 bigint, Dir10 bigint);
+CREATE TABLE rooms( 
+       RoomNumber bigint PRIMARY KEY,
+       ShortDesc VARCHAR(82) NOT NULL,
+       LongDesc VARCHAR(322) NOT NULL,
+       north INTEGER NOT NULL,
+       northeast INTEGER NOT NULL,
+       east INTEGER NOT NULL,
+       southeast INTEGER NOT NULL,
+       south INTEGER NOT NULL,
+       southwest INTEGER NOT NULL,
+       west INTEGER NOT NULL,
+       northwest INTEGER NOT NULL,
+       up INTEGER NOT NULL,
+       down INTEGER NOT NULL
+);
 
-insert into rooms values( 1, "Room dia Muertos", "The stale air and dank stone make it difficult to breathe. Bones lay piled, who knows how deep they are. Many have died here and more come each day.",0,0,0,0,0,0,0,0,2,0);
 
-create table races( RaceNumber int primary key, RaceName varchar(32), RaceDesc varchar(322));
+CREATE TABLE races(
+       RaceNumber int PRIMARY KEY,
+       RaceName VARCHAR(32) NOT NULL,
+       RaceDesc VARCHAR(322) NOT NULL
+);
 
-insert into races values( 1, "Human", "The Human is the most balanced race at this time.");
-insert into races values( 1, "Dark-Elf", "The Dark-Elves are a darker relative of the Elf.");
-insert into races values( 2, "Dwarf", "The Dwarves are a stout and healthy race, what they lack in height they make up for with sturdiness and brawn.");
 
-create table classes( ClassNumber int primary key, ClassName varchar(32), ClassDesc varchar(322));
 
-insert into classes values( 1, "Fighter", "Physical Combat is thier forte.");
-insert into classes values( 2, "Ranger", "Drawn to the woodlands and thier protection, Rangers gain special powers from Nature.");
-insert into classes values( 1, "Cleric", "Physical Combat is thier holy calling, and healing thier forte.");
-insert into classes values( 1, "Mage", "Frail physically but able to concentrate the mysterious energies into deadly forces.");
+CREATE TABLE classes(
+       ClassNumber int PRIMARY KEY,
+       ClassName VARCHAR(32) NOT NULL,
+       ClassDesc VARCHAR(322) NOT NULL
+);
+
